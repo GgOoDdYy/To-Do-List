3260baa6933d4f39fbfa8cab955817a494037348
Goodman <goodmanndahiriwe005@gmail.com>
2024-07-19 21:43:19 +0300

    updatedone

--- script.js
+++ script.js
@@ -3,14 +3,29 @@ const btn = document.getElementById("essentials");
 const input = document.getElementById("textInput");
 
 btn.addEventListener("click", function () {
-  container.innerHTML += `
-                          <div class="item"><li>${input.value}</li> 
-                           <button class="removebtn">Remove</button>
-                           </div>`;
+  if (input.value !== "") {
+    container.innerHTML += `
+                              <div class="item"><li>${input.value}</li> 
+                              <button class="removebtn">Remove</button>
+                              </div>`;
+    input.value = "";
+  }
 });
 
 container.addEventListener("click", function (e) {
   if (e.target && e.target.classList.contains("removebtn")) {
     e.target.parentElement.remove("item");
+  }
+});
+
+input.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    if (input.value !== "") {
+      container.innerHTML += `
+                                <div class="item"><li>${input.value}</li> 
+                                <button class="removebtn">Remove</button>
+                                </div>`;
+      input.value = "";
+    }
   }
 });
